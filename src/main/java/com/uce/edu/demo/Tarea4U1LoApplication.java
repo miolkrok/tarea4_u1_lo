a4f36b01b978e51606a762c31cb702a8cdeae720
Luis Ortiz <luchoaam97@gmail.com>
2022-05-29 23:57:13 -0500

    commit de las clases creadas

---
+++ src/main/java/com/uce/edu/demo/Tarea4U1LoApplication.java
@@ -0,0 +1,20 @@
+package com.uce.edu.demo;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tarea4U1LoApplication implements CommandLineRunner{
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tarea4U1LoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/main/java/com/uce/edu/demo/matriculacion/Materia.java
@@ -0,0 +1,30 @@
+package com.uce.edu.demo.matriculacion;
+
+public class Materia {
+
+	private String nombreMateria;
+	private String semestre;
+	private String aula;
+	
+	//SET Y GET
+	public String getNombreMateria() {
+		return nombreMateria;
+	}
+	public void setNombreMateria(String nombreMateria) {
+		this.nombreMateria = nombreMateria;
+	}
+	public String getSemestre() {
+		return semestre;
+	}
+	public void setSemestre(String semestre) {
+		this.semestre = semestre;
+	}
+	public String getAula() {
+		return aula;
+	}
+	public void setAula(String aula) {
+		this.aula = aula;
+	}
+	
+	
+}
