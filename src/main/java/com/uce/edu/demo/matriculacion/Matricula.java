5ab31ea8cf0a3c59d8c15f532c8f2c25ffb60d4e
Luis Ortiz <luchoaam97@gmail.com>
2022-05-29 23:57:08 -0500

    commit de las clases creadas

---
+++ src/main/java/com/uce/edu/demo/matriculacion/Matricula.java
@@ -0,0 +1,22 @@
+package com.uce.edu.demo.matriculacion;
+
+public class Matricula {
+	
+	private Estudiante estudiante;
+	private Materia materia;
+	
+	//SET Y GET
+	public Estudiante getEstudiante() {
+		return estudiante;
+	}
+	public void setEstudiante(Estudiante estudiante) {
+		this.estudiante = estudiante;
+	}
+	public Materia getMateria() {
+		return materia;
+	}
+	public void setMateria(Materia materia) {
+		this.materia = materia;
+	}
+
+}
